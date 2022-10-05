6d2705c0751c76da5409becc1b481a5fd613e5a0
Qavi-Nizamani <Qaviniz11@gmail.com>
2022-10-05 15:54:35 +0500

    Complete

--- pages/[uri].js
+++ pages/[uri].js
@@ -1,9 +1,8 @@
-import Head from 'next/head'
-import Footer from '../components/Footer'
-import { getPostByUri } from '../lib/test-data';
-
+import Head from 'next/head';
+import Footer from '../components/Footer';
+import { client } from '../lib/apollo';
+import { gql } from '@apollo/client/core';
 export default function SlugPage({ post }) {
-
   return (
     <div>
       <Head>
@@ -12,37 +11,58 @@ export default function SlugPage({ post }) {
       </Head>
 
       <main>
-          <div className="siteHeader">
-            <h1 className="title">
-                {post.title}
-            </h1>
-            <p>✍️  &nbsp;&nbsp;{`${post.author.node.firstName} ${post.author.node.lastName}`} | 🗓️ &nbsp;&nbsp;{ new Date(post.date).toLocaleDateString() }</p>
-          </div>
-            <article dangerouslySetInnerHTML={{__html: post.content}}>   
-            </article>
+        <div className="siteHeader">
+          <h1 className="title">{post.title}</h1>
+          <p>
+            ✍️ &nbsp;&nbsp;{`${post.author.node.firstName} ${post.author.node.lastName}`} | 🗓️
+            &nbsp;&nbsp;{new Date(post.date).toLocaleDateString()}
+          </p>
+        </div>
+        <article dangerouslySetInnerHTML={{ __html: post.content }}></article>
       </main>
 
       <Footer></Footer>
-
     </div>
-  )
+  );
 }
 
+export async function getStaticProps({ params }) {
+  const GET_POST = gql`
+    query GetAllPosts($id: ID!) {
+      post(id: $id, idType: URI) {
+        author {
+          node {
+            firstName
+            lastName
+          }
+        }
+        date
+        content
+        title
+      }
+    }
+  `;
+  // const response = await getPostByUri(params.uri)
+  const response = await client.query({
+    query: GET_POST,
+    variables: {
+      id: params.uri,
+    },
+  });
 
-export async function getStaticProps({ params }){
-  const response = await getPostByUri(params.uri)
-  const post = response?.data?.post
+  const post = response?.data?.post;
+
   return {
     props: {
-      post
-    }
-  }
+      post,
+    },
+  };
 }
 
-export async function getStaticPaths(){
-    const paths = []
-    return {
-        paths,
-        fallback: 'blocking'
-    }
+export async function getStaticPaths() {
+  const paths = [];
+  return {
+    paths,
+    fallback: 'blocking',
+  };
 }
